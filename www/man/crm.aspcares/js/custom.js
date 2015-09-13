ee79fc9160f78a54e0de8a9233841d82062ffd12
Shazil Shafiq <wdshazil@gmail.com>
2015-09-14 01:58:59 +0500

    done

---
+++ www/man/crm.aspcares/js/custom.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+ 
+ $(document).ready(function() {
+ 
+	$(window).load(function(){
+		
+		$('.login_effect').removeClass('login_effect').addClass('login_effectJQ');
+		
+	});
+ 
+ });
